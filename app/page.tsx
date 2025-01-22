b9db47700383a8f92f391639aaee3f7fee662dfb
KIRAN <ki2003167@gmail.com>
2025-01-22 17:45:16 +0530

    22nd

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
       <section className="space-y-8 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 text-center">
         <div className="container max-w-[64rem] space-y-6">
           <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight">
-            Hi, I'm <span className="text-primary"><div className="err">R Kiran Kumar Reddy</div></span>
+            Hi, I'm <span className="text-primary" style={{color:"red"}}>R Kiran Kumar Reddy</span>
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl sm:leading-8">
             I'm a <b>Generative-AI</b> developer passionate about building <b>AI Agents</b> and real-time AI applications. 
